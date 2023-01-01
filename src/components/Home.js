6cb0e10d5e14cdd25d454b4087e4b029b7478923
Devillllllllllll <onuchit037@gmail.com>
2023-01-02 01:53:13 +0700

    edit t 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,32 @@ import navbars from './Navbars'
 const Home = () => {
     const { currentUser } = useContext(AuthContext);
 
+    const people = [
+        {
+          name: 'Calvin Hawkins',
+          email: 'calvin.hawkins@example.com',
+          image:
+            'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Kristen Ramos',
+          email: 'kristen.ramos@example.com',
+          image:
+            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Ted Fox',
+          email: 'ted.fox@example.com',
+          image:
+            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      ]
+
     return (
         <>
-            <div>
-                {navbars}
-            </div>
+
             <div className="container mt-5">
+                {navbars}
                 <h1>Home</h1>
                 {currentUser ? (
                     <p>You are logged in - <Link to="/dashboard">View Dashboard</Link></p>
@@ -20,6 +40,20 @@ const Home = () => {
                         <Link to="/login" className="btn btn-primary">Log In</Link> or <Link to="/signup" className="btn btn-success">Sign Up</Link>
                     </p>
                 )}
+            </div>
+
+            <div>
+                <ul className="divide-y divide-gray-200">
+                    {people.map((person) => (
+                        <li key={person.email} className="py-4 flex">
+                        <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+                            <div className="ml-3">
+                                <p className="text-sm font-medium text-gray-900">{person.name}</p>
+                                <p className="text-sm text-gray-500">{person.email}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </>
     )
