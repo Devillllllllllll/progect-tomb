8dcc627c67ba2f23063ef6bcd000f80435b03fad
Devillllllllllll <onuchit037@gmail.com>
2023-01-02 17:39:29 +0700

    set Navbars succesfully in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,34 +4,18 @@ import { AuthContext } from './Auth'
 import Navbars from './Navbars'
 import './tailwind.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { AuthProvider } from './Auth'
 
 
 const Home = () => {
     const { currentUser } = useContext(AuthContext);
 
-    const people = [
-        {
-          name: 'Calvin Hawkins',
-          email: 'calvin.hawkins@example.com',
-          image:
-            'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        {
-          name: 'Kristen Ramos',
-          email: 'kristen.ramos@example.com',
-          image:
-            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        {
-          name: 'Ted Fox',
-          email: 'ted.fox@example.com',
-          image:
-            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-      ]
 
     return (
         <>
+            {/* Navbars */}
+            <AuthProvider><Router><Switch><Route path="/" exact><Navbars/></Route></Switch></Router></AuthProvider>
+
             <div className="container mt-5">
                 <h2 class="mb-12 text-center text-5xl font-extrabold">Home Tomb ฌาปนสถาน ธม.</h2>
                 {currentUser ? (
@@ -46,19 +30,7 @@ const Home = () => {
                 )}
             </div>
 
-            <div>
-                <ul className="divide-y divide-gray-200">
-                    {people.map((person) => (
-                        <li key={person.email} className="py-4 flex">
-                        <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                            <div className="ml-3">
-                                <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                                <p className="text-sm text-gray-500">{person.email}</p>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+
         </>
     )
 }
